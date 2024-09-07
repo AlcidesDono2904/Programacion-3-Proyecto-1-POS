5522173a8e8cde185fa9725191e302166962c854
Jeferson-GH <alberto.gh.1824.31@gmail.com>
2024-09-07 12:10:29 -0600

    productoa

--- Pos/src/main/java/pos/presentation/productos/Controller.java
+++ Pos/src/main/java/pos/presentation/productos/Controller.java
@@ -1,4 +1,62 @@
 package pos.presentation.productos;
 
 public class Controller {
+    View view;
+    Model model;
+
+    public Controller(View view, Model model) {
+        model.init(Service.instance().search(new Productos()));
+        this.view = view;
+        this.model = model;
+        view.setController(this);
+        view.setModel(model);
+    }
+
+    // Método de búsqueda con filtro de código o descripción
+    public void search(Productos filter) throws Exception {
+        model.setFilter(filter);
+        model.setMode(Application.MODE_CREATE);
+        model.setCurrent(new Productos());
+        model.setList(Service.instance().search(
+                filter.getCodigo() != null ? filter.getCodigo() : "",
+                filter.getDescripcion() != null ? filter.getDescripcion() : ""
+        ));
+    }
+
+    // Método para guardar un producto (crear o actualizar)
+    public void save(Productos e) throws Exception {
+        switch (model.getMode()) {
+            case Application.MODE_CREATE:
+                Service.instance().create(e);
+                break;
+            case Application.MODE_EDIT:
+                Service.instance().update(e);
+                break;
+        }
+        model.setFilter(new Productos());
+        search(model.getFilter());
+    }
+
+    // Método para editar un producto basado en la fila seleccionada
+    public void edit(int row) {
+        Productos e = model.getList().get(row);
+        try {
+            model.setMode(Application.MODE_EDIT);
+            model.setCurrent(Service.instance().read(e));
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    // Método para eliminar un producto
+    public void delete() throws Exception {
+        Service.instance().delete(model.getCurrent());
+        search(model.getFilter());
+    }
+
+    // Método para limpiar la selección y preparar la creación de un nuevo producto
+    public void clear() {
+        model.setMode(Application.MODE_CREATE);
+        model.setCurrent(new Productos());
+    }
 }
