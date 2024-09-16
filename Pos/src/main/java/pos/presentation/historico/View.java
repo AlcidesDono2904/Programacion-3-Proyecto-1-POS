e6a1b2ad04026f28fcbf593e738f3ecba47c7ae0
Jeferson-GH <alberto.gh.1824.31@gmail.com>
2024-09-16 00:29:57 -0600

    Class hist

--- Pos/src/main/java/pos/presentation/historico/View.java
+++ Pos/src/main/java/pos/presentation/historico/View.java
@@ -3,9 +3,9 @@ package pos.presentation.historico;
 import javax.swing.*;
 
 public class View {
-    private JTextField textField1;
-    private JButton buscarButton;
-    private JButton reporteButton;
-    private JTable table1;
-    private JTable table2;
+    private JTextField buscliente;
+    private JButton btnBuscar;
+    private JButton btnReporte;
+    private JTable list;
+    private JTable lineas;
 }
