942366bebecab2b072395764c4698e9ff7957782
Jeferson-GH <alberto.gh.1824.31@gmail.com>
2024-09-16 00:27:04 -0600

    Historico

---
+++ Pos/src/main/java/pos/presentation/historico/View.java
@@ -0,0 +1,11 @@
+package pos.presentation.historico;
+
+import javax.swing.*;
+
+public class View {
+    private JTextField textField1;
+    private JButton buscarButton;
+    private JButton reporteButton;
+    private JTable table1;
+    private JTable table2;
+}
